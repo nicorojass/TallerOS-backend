36baae811cf40cc0bbb4661f7647c9c127ca42c9
Nico <nicoorojas1@gmail.com>
2026-03-16 15:30:38 -0300

    initial commit

---
+++ src/main/java/com/talleros/TallerosApplication.java
@@ -0,0 +1,13 @@
+package com.talleros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerosApplication.class, args);
+	}
+
+}
